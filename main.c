0d79705d19089392a3198c359ea5ab8d0caeabd5
Simply27 <dawiozol@gmail.com>
2020-04-10 16:44:07 +0200

    reading input starts to work

--- main.c
+++ main.c
@@ -6,53 +6,34 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <ctype.h>
 
-typedef struct parser_params
+void get_data(char** data_member, FILE** data)
 {
-    char *start;
-    char *finish;
-    char delim;
-}parser_params;
+    *data_member = (char*) malloc(128*sizeof(char));
 
-int parser(char *line, char del, char *newstr)
-{
-    if(line)
-    {
-        parser_params params = {.delim = del};
-        params.start = line;
-        params.finish = params.start;
-        while (*params.finish != params.delim || *params.finish != '\0')
-        {
-            ++params.finish;
-        }
+    size_t n = 0;
+    int c;
 
-        newstr = malloc(params.finish - params.start);
-        
-        while(params.start != params.finish)
-        {
-            *newstr = *params.start;
-            ++newstr;
-            ++params.start;
-        }
-    }
-    else
+    while ((c = fgetc(*data)) != ' ')
     {
-        fprintf(stderr, "line is a null pointer!");
-        return(-1);
+        (*data_member)[n++] = (char) c;
     }
-    
+
+    (*data_member)[n] = '\0';
 }
 
 void file_to_vectors()
 {
     FILE *data = fopen("data.txt", "r");
+    if (data == NULL)
+        printf("No data in file!");
 
-    char *file_line = NULL, *data_member = NULL;
-    size_t file_line_len, data_member_len;
+    char *data_member = NULL;
 
-    getline(&file_line, &data_member_len, data);
-    parser(file_line, ' ', data_member);
-    printf("%s", file_line);
+    get_data(&data_member, &data);
+
+    printf("%s", data_member);
 }
 
 int main()
