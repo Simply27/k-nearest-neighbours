212494ad827192cf3ebfaf233a66348b4ead5168
Simply27 <dawid.warzecha@onet.pl>
2020-04-10 15:35:47 +0200

    testing the parser algorithm

--- main.c
+++ main.c
@@ -4,41 +4,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
+
+typedef struct parser_params
+{
+    char *start;
+    char *finish;
+    char delim;
+}parser_params;
+
+int parser(char *line, char del, char *newstr)
+{
+    if(line)
+    {
+        parser_params params = {.delim = del};
+        params.start = line;
+        params.finish = params.start;
+        while (*params.finish != params.delim || *params.finish != '\0')
+        {
+            ++params.finish;
+        }
+
+        newstr = malloc(params.finish - params.start);
+        
+        while(params.start != params.finish)
+        {
+            *newstr = *params.start;
+            ++newstr;
+            ++params.start;
+        }
+    }
+    else
+    {
+        fprintf(stderr, "line is a null pointer!");
+        return(-1);
+    }
+    
+}
 
 void file_to_vectors()
 {
     FILE *data = fopen("data.txt", "r");
 
-    int i = 0;
-    char c, *data_member;
-
-    while(c != ' ')
-    {
-        c = fgetc(data);
-        data_member = (char *) realloc(data_member, sizeof(char));
-        data_member[i] = c;
-        ++i;
-    }
-
-    printf("%s", data_member);
-
-    free(data_member);
-    fclose(data);
-
-    /*
     char *file_line = NULL, *data_member = NULL;
     size_t file_line_len, data_member_len;
 
-    while(getdelim(&file_line, &data_member_len, '\n', data) != -1)
-    {
-        while()
-        {
-            printf("%s ", data_member);
-        }
-        printf("\n");
-    }
-    */
+    getline(&file_line, &data_member_len, data);
+    parser(file_line, ' ', data_member);
+    printf("%s", file_line);
 }
 
 int main()
