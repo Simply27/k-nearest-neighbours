9a6376e4387795e5284308ce7955a2777be6e923
Simply27 <dawid.warzecha@onet.pl>
2020-04-12 15:04:44 +0200

    user input pretty secure and smart now, spaces error to fix

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 /*
-- warn about typos
-- compare size of vectors
+TODO
+
+Input:
+- error -> inputting 0 when space at the end or begining of vector
+- cast to char macro?
+- file to vectors is a temporary method
 - input tests
+- ask for vector
 */
 
 #include <stdio.h>
@@ -11,48 +16,85 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+typedef enum read_error
+{
+    NO_ERROR,
+    LETTER_AFTER_NUM,
+    DIFFERENT_VECTOR_DIMENSION
+} read_error;
+
 typedef struct data_info
 {
     size_t varray_size, vector_size;
     int c;
+    read_error error;
 } data_info;
 
-int get_data_member(char** vector_element, FILE* data)
+void check_for_errors(int err)
+{
+    if (err == LETTER_AFTER_NUM)
+    {
+        printf("It looks like there are some typos in your data, "
+               "which were ignored. The calculation will proceed, but "
+               "please check your input to ensure the results are not flawed."
+               "\n\nPress any key to continue...");
+        getchar();
+    }
+    else if (err == DIFFERENT_VECTOR_DIMENSION)
+    {
+        printf("The vectors are not of the same dimension. "
+               "Please check your input."
+               "\nThe program will now terminate."
+               "\n\nPress any key to continue...");
+        getchar();
+        exit(EXIT_FAILURE);
+    }
+}
+
+data_info get_data_member(char** vector_element, FILE* data)
 {
     int buffer_size = 128;
 
+    data_info info = {sizeof(float*), sizeof(float), 'a', NO_ERROR};
+
     /* initialized as 'a' since letters are ignored by default */
-    int c = 'a', prev = 'a', prev_2 = 'a', prev_3 = 'a';
+    int prev = 'a', prev_2 = 'a', prev_3 = 'a';
 
     size_t element_iter = 0;
-    bool is_exp = false;
+    bool is_exp = false, was_digit = false;
 
     *vector_element = (char*) malloc(buffer_size * sizeof(char));
 
-    while (c != ' ' && c != ',' && c != '\n' && c != EOF)
+    while (info.c != ' ' && info.c != ',' && info.c != '\n' && info.c != EOF)
     {
         prev_3 = prev_2;
         prev_2 = prev;
-        prev = c;
-        c = getc(data);
+        prev = info.c;
+        info.c = getc(data);
 
-        if (((isdigit((char) prev_2))
-            && ((char) prev == '.' || (char) prev == 'e')
-            && isdigit((char) c) && !is_exp)
-            || (!(isdigit((char) prev_2)) && (char) prev == '-'
-            && isdigit((char) c)))
+        if ((isdigit((char) prev_2)
+            && (prev == '.' || prev == 'e')
+            && isdigit((char) info.c) && !is_exp)
+            || (!isdigit((char) prev_2) && prev == '-'
+            && isdigit((char) info.c)))
         {
             (*vector_element)[element_iter++] = (char) prev;
-            (*vector_element)[element_iter++] = (char) c;
+            (*vector_element)[element_iter++] = (char) info.c;
 
             if (!is_exp)
             {
                 is_exp = true;
             }
         }
-        else if (isdigit((char) c))
+        else if (!isdigit((char) info.c) && info.c != 'e' && info.c != ' '
+                 && info.c != '\n' && info.c != EOF)
         {
-            (*vector_element)[element_iter++] = (char) c;
+            info.error = LETTER_AFTER_NUM;
+        }
+        else if (isdigit((char) info.c))
+        {
+            (*vector_element)[element_iter++] = (char) info.c;
+            was_digit = true;
         }
         
 
@@ -69,7 +111,7 @@ int get_data_member(char** vector_element, FILE* data)
     *vector_element = realloc(*vector_element,
                             (strlen(*vector_element) + 1) * sizeof(char));
     
-    return c;
+    return info;
 }
 
 data_info get_vector(float** vector, FILE* data)
@@ -77,16 +119,16 @@ data_info get_vector(float** vector, FILE* data)
     int buffer_size = 128;
 
     /* initialized as 'a' since letters are ignored by default */
-    int c = 'a';
+    data_info info = {sizeof(float*), sizeof(float), 'a', NO_ERROR};
 
     size_t vector_iter = 0;
 
     *vector = (float*) malloc(buffer_size * sizeof(float));
 
-    while (c != '\n' && c != EOF)
+    while (info.c != '\n' && info.c != EOF)
     {
         char* vector_element;
-        c = get_data_member(&vector_element, data);
+        info = get_data_member(&vector_element, data);
 
         (*vector)[vector_iter++] = strtof(vector_element, NULL);
         free(vector_element);
@@ -99,7 +141,7 @@ data_info get_vector(float** vector, FILE* data)
 
     *vector = realloc(*vector, vector_iter * sizeof(float));
 
-    data_info info = { sizeof(float*), vector_iter, c };
+    info.vector_size = vector_iter;
 
     return info;
 }
@@ -109,9 +151,9 @@ data_info get_varray(float*** varray, FILE* data)
     int buffer_size = 128;
 
     /* c initialized as 'a' since letters are ignored by default */
-    data_info info = {sizeof(float*), sizeof(float), 'a'};
+    data_info info = {sizeof(float*), sizeof(float), 'a', NO_ERROR};
 
-    size_t varray_iter = 0;
+    size_t varray_iter = 0, prev_vector_size;
 
     *varray = (float**) malloc(buffer_size * sizeof(float*));
 
@@ -119,6 +161,13 @@ data_info get_varray(float*** varray, FILE* data)
     {
         float* vector;
         info = get_vector(&vector, data);
+
+        if ((varray_iter != 0) && (info.vector_size != prev_vector_size))
+        {
+            info.error = DIFFERENT_VECTOR_DIMENSION;
+        }
+
+        prev_vector_size = info.vector_size;
 
         (*varray)[varray_iter++] = vector;
 
@@ -142,6 +191,8 @@ void file_to_vectors()
     float ** varray;
 
     data_info info = get_varray(&varray, data);
+
+    check_for_errors(info.error);
 
     for (size_t i = 0; i < info.varray_size; ++i)
     {
